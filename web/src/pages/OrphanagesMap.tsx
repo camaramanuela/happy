af3d4bc5d520cd354a44eebd21803adc39bd4e3f
Manuela Bognar <manuelabcamara@gmail.com>
2020-10-12 17:38:03 -0300

    page with map

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
+import '../styles/pages/orphanages-map.css';
+
+import mapMarkerImg from '../images/map-marker.svg';
 
 function OrphanagesMap() {
   return (
-    <h1>orphanages map</h1>
+    <div id="page-map"> 
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperndo a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>São Paulo</strong>
+          <span>São Paulo</span>
+        </footer>
+      </aside>
+
+      <Map 
+        center={[-23.587416,-46.659150]}
+        zoom={16}
+        style={{ width: '100%', height: '100%' }}  
+      >
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </Map>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
   );
 }
 
