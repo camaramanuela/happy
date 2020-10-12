f95d2cbfd9fe45cc103e1a70594ed153e7ecfb98
Manuela Bognar <manuelabcamara@gmail.com>
2020-10-12 15:42:00 -0300

    landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+import logoImg from './images/logo.svg';
+import landing from './images/landing.svg';
 
 function App() {
   return (
-    <h1>start</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="happy"/>
+        
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visiste orfanatos e mude o dia de muitas crianças</p>
+        </main>
+
+        <div className="location">
+          <strong>São Paulo</strong>
+          <span>São Paulo</span>
+        </div>
+
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0,0,0,0.6" />
+        </a>
+
+      </div>
+    </div>
   );
 }
 
